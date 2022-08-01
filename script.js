deee24332eb13686c51ff4ebf39f7e7b34ad5958
Adeeba Tabassum <92844344+Adeeba002@users.noreply.github.com>
2022-08-01 18:53:34 +0530

    Calculator

--- script.js
+++ script.js
@@ -57,3 +57,13 @@ $('#two').click(function(){
                                                         });
                                                 
                                                 })
+
+                                                function loader(){
+                                                    document.querySelector('.loader-container').classList.add('fade-out');
+                                                  }
+                                                  
+                                                  function fadeOut(){
+                                                    setInterval(loader, 3000);
+                                                  }
+                                                  
+                                                  window.onload = fadeOut;
